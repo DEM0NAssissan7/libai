66ef1d25054b120f56dcef5f620ea9a53a075fa7
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2024-04-30 21:34:26 -0500

    fix things

--- C/libai.c
+++ C/libai.c
@@ -11,54 +11,53 @@ float inputs[4][2] = {
     {0, 1},
     {1, 1}
 };
-float targets[4][1] = {
-    {0},
-    {1},
-    {1},
-    {0}
+float targets[4] = {
+    0,
+    1,
+    1,
+    0
 };
 
 long trials = 0;
 
-void r_array(int len, float *a, float *b){
+void write_array(int len, float *a, float *b){
     for(int i = 0; i < len; i++)
         a[i] = b[i];
 }
 void set_io(){
     int index = trials % 4;
-    r_array(2, input, inputs[index]);
-    r_array(2, target, targets[index]);
+    write_array(2, input, inputs[index]);
+    target[0] = targets[index];
     trials++;
 }
-
+void test(Network *network){
+    for(int i = 0; i < 4; i++){
+        set_io();
+        netrun(network, input);
+        netprintout(network);
+    }
+}
 
 int main(){
     //Inits
 
     //Program begin
     Layout layout = netlay(2, 2, 1);
-    Network network = netinit(&layout, 4, 0.07);
+    Network network = netinit(&layout, 4, 0.02);
 
-    void train(){
+
+    printf("Before training:\n");
+    test(&network);
+    
+    for(int i = 0; i < 4; i++) {
         set_io();
+        printf("Input: (%f, %f) | Target: (%f)\n", input[0], input[1], target[0]);
         nettrain(&network, input, target);
     }
-    void test(){
-        for(int i = 0; i < 4; i++){
-            set_io();
-            netrun(&network, input);
-            netprintout(&network);
-        }
-    }
-    printf("Before training:\n");
-    test();
-    
-    for(int i = 0; i < 1000000; i++)
-        train();
 
 
     printf("\nAfter training:\n");
     netdebug(&network);
-    test();
+    test(&network);
     return 0;
 }
